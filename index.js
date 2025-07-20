c09472af62d32f82949b2e7d8803f6f364869065
Rozza <r.sarantola@gmail.com>
2025-07-20 15:21:05 +0300

    Added reverse chronological ordering, formatting

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const displayElement = document.getElementById('json-data');
 displayElement.style.fontSize = '18px';
-fetch('./sample-data/entries.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+fetch('./sample-data/data.json')
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    let text = "";
+    while (data.length>0)
+    {
+        let newestDate = new Date("1990-01-01");
+        let currentData;
+        for (let i = 0; i<=data.length-1; i++)
+        {
+            if (newestDate.getTime()<new Date(data[i].isoTime).getTime())
+            {
+                currentData=i;
+                newestDate=new Date(data[i].isoTime);
+            }
         }
-        return response.json();  })
-    .then(data => {
-        console.log(data)})
+        text += `<h4><br>${data[currentData].title}</h4><p>${data[currentData].body}<br>Time: ${new Date(data[currentData].isoTime).toString()}<br>Latitude: ${data[currentData].lat}<br>Longitude: ${data[currentData].lon}<br></p>`;
+        data.splice(currentData, 1);
+    }
+    displayElement.innerHTML = text;
+})
