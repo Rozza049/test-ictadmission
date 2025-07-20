150c7b38d3584aecfc1b3d1e039876ca053eeaf7
Rozza <r.sarantola@gmail.com>
2025-07-20 15:58:14 +0300

    Initial adding of modal, further functionality to be added

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ fetch('./sample-data/data.json')
     }
     displayElement.innerHTML = text;
 })
+
+var modal = document.getElementById("inputModal");
+var btn = document.getElementById("modalButton");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
