bc76ebef007dddeb450dfe079a9b43279f91ff67
Rozza <r.sarantola@gmail.com>
2025-07-26 16:40:37 +0300

    Finished the initial requirements

--- index.js
+++ index.js
@@ -1,39 +1,90 @@
 const displayElement = document.getElementById('json-data');
 displayElement.style.fontSize = '18px';
+
+var modal = document.getElementById("inputmodal");
+var entrBtn = document.getElementById("acceptentry")
+var modBtn = document.getElementById("modalbutton");
+var span = document.getElementsByClassName("close")[0];
+var error = document.getElementById("entryerror");
+
+var title = document.getElementById("title");
+var desc = document.getElementById("description");
+var lat = document.getElementById("latitude");
+var lon = document.getElementById("longitude");
+lon.value = "";
+lat.value = "";
+title.value = "";
+desc.value = "";
+
+let count = 1;
+
 fetch('./sample-data/data.json')
 .then(response => response.json())
-.then(data => {
-    console.log(data);
+.then(data => listEntries(data))
+
+function listEntries(entries) {
     let text = "";
-    while (data.length>0)
+    count += entries;
+    while (entries.length>0)
     {
         let newestDate = new Date("1990-01-01");
         let currentData;
-        for (let i = 0; i<=data.length-1; i++)
+        for (let i = 0; i<=entries.length-1; i++)
         {
-            if (newestDate.getTime()<new Date(data[i].isoTime).getTime())
+            if (newestDate.getTime()<new Date(entries[i].isoTime).getTime())
             {
                 currentData=i;
-                newestDate=new Date(data[i].isoTime);
+                newestDate=new Date(entries[i].isoTime);
             }
         }
-        text += `<h4><br>${data[currentData].title}</h4><p>${data[currentData].body}<br>Time: ${new Date(data[currentData].isoTime).toString()}<br>Latitude: ${data[currentData].lat}<br>Longitude: ${data[currentData].lon}<br></p>`;
-        data.splice(currentData, 1);
-    }
-    displayElement.innerHTML = text;
-})
+        text += `<h4><br>${entries[currentData].title}</h4><p>${entries[currentData].body}<br>Time: ${new Date(entries[currentData].isoTime).toString()}<br>Latitude: ${entries[currentData].lat}<br>Longitude: ${entries[currentData].lon}<br></p>`;
+        entries.splice(currentData, 1);
 
-var modal = document.getElementById("inputModal");
-var btn = document.getElementById("modalButton");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
+    }
+    text += displayElement.innerHTML
+    displayElement.innerHTML = text;
+}
+
+modBtn.onclick = function() {
     modal.style.display = "block";
 }
 span.onclick = function() {
     modal.style.display = "none";
+    lon.value = "";
+    lat.value = "";
+    title.value = "";
+    desc.value = "";
 }
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        lon.value = "";
+        lat.value = "";
+        title.value = "";
+        desc.value = "";
+    }
+}
+
+entrBtn.onclick = function() {
+    var date = Date.now();
+    var newEntry;
+    if (title.value == "" || desc.value == "" || (!(lon.value == "" && lat.value == "") && !(lon.value != "" && lat.value != "" && !isNaN(lat.value) && !isNaN(lon.value)))) {
+        error.innerHTML = "Error, invalid input";
+    } else {
+        error.innerHTML = "";
+        if (lon.value == "")
+        {
+            lon.value = "null";
+            lat.value = "null";
+        }
+        count = count + 1;
+        newEntry={"id": count, "title": title.value, "body": desc.value, "isoTime": date, "lat": lat.value, "lon": lon.value};
+        let text = `<h4><br>${newEntry.title}</h4><p>${newEntry.body}<br>Time: ${new Date(newEntry.isoTime).toString()}<br>Latitude: ${newEntry.lat}<br>Longitude: ${newEntry.lon}<br></p>` + displayElement.innerHTML;
+        displayElement.innerHTML = text;
+        modal.style.display = "none";
+        lon.value = "";
+        lat.value = "";
+        title.value = "";
+        desc.value = "";
     }
 }
